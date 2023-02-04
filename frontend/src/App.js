ed846a8953053759774ed76c536d35e5d4053603
Colby <Colbyx20@gmail.com>
2023-02-04 13:46:34 -0500

    f

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import {Routes, Route} from "react-router-dom";
+import './App.css';
 import Navbar from './components/navbar';
 import Button from './components/button';
 import Home from './pages/home';
